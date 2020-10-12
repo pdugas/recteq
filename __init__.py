38655126f47c8361098324ed6866623a0ab8b5ca
Paul Dugas <paul@dugasenterprises.com>
2020-10-12 08:53:01 -0400

    shutdown error

--- __init__.py
+++ __init__.py
@@ -63,7 +63,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
         )
     )
     if unload_ok:
-        await hass.data[DOMAIN].pop(entry.entry_id).shutdown()
+        hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
 
